be32d98d0e9b46f12fe25f0878a0e2aadcae70cd
AbsharHassan <abshar.hassan7@gmail.com>
2023-11-16 08:49:15 +0500

    disabled react.strictmode to prevent clash with google maps api

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,15 +12,9 @@ if (process.env.NODE_ENV === 'production') disableReactDevTools()
 const root = createRoot(document.getElementById('root'))
 
 root.render(
-  // <React.StrictMode>
   <Router>
     <Provider store={store}>
       <App />
     </Provider>
   </Router>
-  // </React.StrictMode>
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
