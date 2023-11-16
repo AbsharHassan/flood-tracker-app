8418519982e6535be5c23f6ba2628f48dd007946
AbsharHassan <abshar.hassan7@gmail.com>
2023-11-16 07:33:38 +0500

    set default date to aug 2022, cleared redundant functions

--- frontend/src/features/apiData/apiDataSlice.js
+++ frontend/src/features/apiData/apiDataSlice.js
@@ -6,159 +6,18 @@ import { geoFormattedPolygons } from '../../MapModification/geoFormattedPolygon'
 import dayjs from 'dayjs'
 
 const initialState = {
-  isLoadingPolygons: true,
-  isFetchingApiData: true,
   isLoadingMainData: true,
   isLoadingTotalFlooded: true,
-  geoFormattedPolygons: geoFormattedPolygons,
-  districtNames: districtNames,
-  // selectedPeriod: 8,
-  completeFloodData: [],
+  geoFormattedPolygons,
+  districtNames,
   selectedFloodData: null,
   prevPeriodFloodData: null,
-  globalSelectedDistrictName: null,
   globalSelectedDistrict: null,
   prevPeriodGlobalSelectedDistrict: null,
   globalSelectedGeometry: null,
-  tryAgain: false,
   selectedDate: '2022-03-01',
-  defaultDateValue: '2022-03-01',
+  defaultDateValue: '2022-08-01',
   totalFloodedArray: [],
-}
-
-// Prepare API flood data for Google Maps API
-const formatFloodData = (data) => {
-  const singlePeriodProcessor = (periodObj) => {
-    const totalArea = periodObj.districts.reduce((sum, curObj) => {
-      return sum + curObj.results.total
-    }, 0)
-
-    const intermediateArray = periodObj.districts.map((innerObject) => {
-      const roadCoords = innerObject.results.roads.map((coords) => {
-        return { lat: coords[1], lng: coords[0] }
-      })
-      innerObject.results.roads = roadCoords
-      return {
-        name: innerObject.name,
-        results: innerObject.results,
-        ratio: innerObject.results.total / totalArea,
-      }
-    })
-
-    let totalFlooded = 0
-    let totalFarmlandAffected = 0
-    let totalUrbanAffected = 0
-    let totalRoadsAffected = 0
-
-    for (let index = 0; index < intermediateArray.length; index++) {
-      totalFlooded +=
-        intermediateArray[index].results.after.floodWater *
-        intermediateArray[index].ratio
-
-      totalFarmlandAffected +=
-        (intermediateArray[index].results.before.farmland -
-          intermediateArray[index].results.after.farmland) *
-        intermediateArray[index].ratio
-
-      totalUrbanAffected +=
-        (intermediateArray[index].results.before.urban -
-          intermediateArray[index].results.after.urban) *
-        intermediateArray[index].ratio
-
-      totalRoadsAffected += intermediateArray[index].results.roads.length
-    }
-
-    const floodValuesArray = intermediateArray.map((district) => {
-      return district.results.after.floodWater
-        ? district.results.after.floodWater
-        : 0
-    })
-
-    const maxFlood = Math.max(...floodValuesArray)
-
-    return {
-      resultsArray: intermediateArray,
-      maxFlood,
-      totalArea,
-      totalFlooded,
-      totalFarmlandAffected,
-      totalUrbanAffected,
-      totalRoadsAffected,
-    }
-  }
-
-  const formattedData = data.map((periodObj, index) => {
-    const results = singlePeriodProcessor(periodObj)
-
-    return {
-      results,
-      after_START: periodObj.after_START,
-      after_END: periodObj.after_END,
-    }
-  })
-
-  return formattedData
-}
-
-const singlePeriodProcessor = (periodObj) => {
-  const totalArea = periodObj.districts.reduce((sum, curObj) => {
-    return sum + curObj.results.total
-  }, 0)
-
-  const intermediateArray = periodObj.districts.map((innerObject) => {
-    const roadCoords = innerObject.results.roads.map((coords) => {
-      return { lat: coords[1], lng: coords[0] }
-    })
-    innerObject.results.roads = roadCoords
-    return {
-      name: innerObject.name,
-      results: innerObject.results,
-      ratio: innerObject.results.total / totalArea,
-    }
-  })
-
-  let totalFlooded = 0
-  let totalFarmlandAffected = 0
-  let totalUrbanAffected = 0
-  let totalRoadsAffected = 0
-
-  for (let index = 0; index < intermediateArray.length; index++) {
-    totalFlooded +=
-      intermediateArray[index].results.after.floodWater *
-      intermediateArray[index].ratio
-
-    totalFarmlandAffected +=
-      (intermediateArray[index].results.before.farmland -
-        intermediateArray[index].results.after.farmland) *
-      intermediateArray[index].ratio
-
-    totalUrbanAffected +=
-      (intermediateArray[index].results.before.urban -
-        intermediateArray[index].results.after.urban) *
-      intermediateArray[index].ratio
-
-    totalRoadsAffected += intermediateArray[index].results.roads.length
-  }
-
-  const floodValuesArray = intermediateArray.map((district) => {
-    return district.results.after.floodWater
-      ? district.results.after.floodWater
-      : 0
-  })
-
-  const maxFlood = Math.max(...floodValuesArray)
-
-  return {
-    resultsArray: intermediateArray,
-    maxFlood,
-    totalArea,
-    totalFlooded,
-    totalFarmlandAffected,
-    totalUrbanAffected,
-    totalRoadsAffected,
-    after_START: periodObj.after_START,
-    after_END: periodObj.after_END,
-  }
 }
 
 // Get districts' flood data
@@ -192,7 +51,6 @@ export const apiDataSlice = createSlice({
   initialState,
   reducers: {
     selectDistrict: (state, { payload }) => {
-      state.globalSelectedDistrictName = payload
       state.globalSelectedDistrict = payload
         ? state.selectedFloodData.resultsArray.find(
             (floodObj) => floodObj.name === payload
@@ -211,42 +69,12 @@ export const apiDataSlice = createSlice({
           )
         : null
     },
-    // delete this and other unnceccesarry code
-
-    setSelectedPeriod: (state, { payload }) => {
-      console.log('set selected PERIOD called')
-      // state.selectedPeriod = payload
-      state.selectedFloodData = state.completeFloodData[payload]
-      state.prevPeriodFloodData = state.completeFloodData[payload - 1]
-
-      state.globalSelectedDistrict = state.globalSelectedDistrictName
-        ? state.completeFloodData[payload].results.resultsArray.find(
-            (floodObj) => floodObj.name === state.globalSelectedDistrictName
-          )
-        : null
-
-      if (payload > 0) {
-        state.prevPeriodGlobalSelectedDistrict =
-          state.globalSelectedDistrictName
-            ? state.completeFloodData[payload - 1].results.resultsArray.find(
-                (floodObj) => floodObj.name === state.globalSelectedDistrictName
-              )
-            : null
-      } else if (payload === 0) {
-        state.prevPeriodGlobalSelectedDistrict =
-          state.globalSelectedDistrictName
-            ? state.completeFloodData[payload].results.resultsArray.find(
-                (floodObj) => floodObj.name === state.globalSelectedDistrictName
-              )
-            : null
-      }
-    },
   },
   extraReducers: (builder) => {
     builder
+      // getFloodData
       .addCase(getFloodData.pending, (state) => {
         state.isLoadingMainData = true
-        // state.tryAgain = false
       })
       .addCase(getFloodData.fulfilled, (state, { payload }) => {
         state.selectedDate = dayjs(payload.current.after_START).format(
@@ -264,7 +92,6 @@ export const apiDataSlice = createSlice({
         state.isLoadingMainData = false
       })
       .addCase(getFloodData.rejected, (state, { payload }) => {
-        state.completeFloodData = []
         state.selectedFloodData = {}
         state.prevPeriodFloodData = {}
         state.isLoadingMainData = false
